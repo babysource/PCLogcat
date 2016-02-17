9c0b900777bc4bbddf78041a959ac4980d5e0289
Wythe <wythe@189.cn>
2016-02-17 11:06:36 +0800

    1.添加工程

---
+++ PCLogcat/LogMain.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace PCLogcat
+{
+    partial class LogMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (this.droidplay != null)
+                {
+                    this.closeListen();
+                }
+                if (this.components != null)
+                {
+                    this.components.Dispose();
+                }
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LogMain));
+            this.logcatBtn = new System.Windows.Forms.Button();
+            this.gridPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.writeLog = new System.Windows.Forms.RichTextBox();
+            this.lockBtn = new System.Windows.Forms.Button();
+            this.queryTxt = new System.Windows.Forms.TextBox();
+            this.queryBtn = new System.Windows.Forms.Button();
+            this.writeTimer = new System.Windows.Forms.Timer(this.components);
+            this.gridPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // logcatBtn
+            // 
+            this.logcatBtn.BackColor = System.Drawing.Color.Green;
+            this.gridPanel.SetColumnSpan(this.logcatBtn, 3);
+            this.logcatBtn.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.logcatBtn.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.logcatBtn.ForeColor = System.Drawing.Color.White;
+            this.logcatBtn.Location = new System.Drawing.Point(3, 3);
+            this.logcatBtn.Name = "logcatBtn";
+            this.logcatBtn.Size = new System.Drawing.Size(978, 59);
+            this.logcatBtn.TabIndex = 0;
+            this.logcatBtn.Text = "开 始 监 听";
+            this.logcatBtn.UseVisualStyleBackColor = false;
+            this.logcatBtn.Click += new System.EventHandler(this.logcatBtn_Click);
+            // 
+            // gridPanel
+            // 
+            this.gridPanel.ColumnCount = 3;
+            this.gridPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.gridPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 120F));
+            this.gridPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 36F));
+            this.gridPanel.Controls.Add(this.logcatBtn, 0, 0);
+            this.gridPanel.Controls.Add(this.writeLog, 0, 1);
+            this.gridPanel.Controls.Add(this.lockBtn, 2, 2);
+            this.gridPanel.Controls.Add(this.queryTxt, 0, 2);
+            this.gridPanel.Controls.Add(this.queryBtn, 1, 2);
+            this.gridPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridPanel.Location = new System.Drawing.Point(0, 0);
+            this.gridPanel.Name = "gridPanel";
+            this.gridPanel.RowCount = 3;
+            this.gridPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 65F));
+            this.gridPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.gridPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 36F));
+            this.gridPanel.Size = new System.Drawing.Size(984, 662);
+            this.gridPanel.TabIndex = 3;
+            // 
+            // writeLog
+            // 
+            this.writeLog.BackColor = System.Drawing.Color.Black;
+            this.gridPanel.SetColumnSpan(this.writeLog, 3);
+            this.writeLog.Cursor = System.Windows.Forms.Cursors.Default;
+            this.writeLog.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.writeLog.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.writeLog.ForeColor = System.Drawing.Color.White;
+            this.writeLog.Location = new System.Drawing.Point(3, 68);
+            this.writeLog.Name = "writeLog";
+            this.writeLog.ReadOnly = true;
+            this.writeLog.Size = new System.Drawing.Size(978, 555);
+            this.writeLog.TabIndex = 4;
+            this.writeLog.Text = "";
+            this.writeLog.WordWrap = false;
+            this.writeLog.GotFocus += new System.EventHandler(this.writeLog_Focus);
+            // 
+            // lockBtn
+            // 
+            this.lockBtn.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lockBtn.Enabled = false;
+            this.lockBtn.Image = global::PCLogcat.Properties.Resources.unlock;
+            this.lockBtn.Location = new System.Drawing.Point(951, 629);
+            this.lockBtn.Margin = new System.Windows.Forms.Padding(3, 3, 3, 6);
+            this.lockBtn.Name = "lockBtn";
+            this.lockBtn.Size = new System.Drawing.Size(30, 27);
+            this.lockBtn.TabIndex = 5;
+            this.lockBtn.UseVisualStyleBackColor = true;
+            this.lockBtn.Click += new System.EventHandler(this.lockBtn_Click);
+            // 
+            // queryTxt
+            // 
+            this.queryTxt.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.queryTxt.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.queryTxt.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.queryTxt.Location = new System.Drawing.Point(3, 631);
+            this.queryTxt.Margin = new System.Windows.Forms.Padding(3, 5, 3, 6);
+            this.queryTxt.Name = "queryTxt";
+            this.queryTxt.Size = new System.Drawing.Size(822, 23);
+            this.queryTxt.TabIndex = 2;
+            // 
+            // queryBtn
+            // 
+            this.queryBtn.BackColor = System.Drawing.Color.Yellow;
+            this.queryBtn.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.queryBtn.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.queryBtn.ForeColor = System.Drawing.Color.Black;
+            this.queryBtn.Location = new System.Drawing.Point(828, 629);
+            this.queryBtn.Margin = new System.Windows.Forms.Padding(0, 3, 0, 6);
+            this.queryBtn.Name = "queryBtn";
+            this.queryBtn.Size = new System.Drawing.Size(120, 27);
+            this.queryBtn.TabIndex = 3;
+            this.queryBtn.Text = "开 始 查 找";
+            this.queryBtn.UseVisualStyleBackColor = false;
+            this.queryBtn.Click += new System.EventHandler(this.queryBtn_Click);
+            // 
+            // writeTimer
+            // 
+            this.writeTimer.Interval = 300;
+            this.writeTimer.Tick += new System.EventHandler(this.writeTimer_Tick);
+            // 
+            // LogMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 662);
+            this.Controls.Add(this.gridPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "LogMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PCLogcat";
+            this.Load += new System.EventHandler(this.LogMain_Load);
+            this.gridPanel.ResumeLayout(false);
+            this.gridPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button logcatBtn;
+        private System.Windows.Forms.TableLayoutPanel gridPanel;
+        private System.Windows.Forms.TextBox queryTxt;
+        private System.Windows.Forms.Button queryBtn;
+        private System.Windows.Forms.RichTextBox writeLog;
+        private System.Windows.Forms.Button lockBtn;
+        private System.Windows.Forms.Timer writeTimer;
+    }
+}
+
---
+++ PCLogcat/LogMain.cs
@@ -0,0 +1,462 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Management;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace PCLogcat
+{
+    public partial class LogMain : Form
+    {
+
+        private static Regex LEVEL_REGEX = new Regex("\\[-*\\d+\\] \\| .+ - ([V|D|I|W|E])/.+: .+ \\r?\\n");
+
+        private int linecount;
+
+        private int drawindex;
+
+        private int findindex;
+
+        private bool isListen;
+
+        private bool isSearch;
+
+        private bool isLocker;
+
+        private string filestore;
+
+        private string cachefile;
+
+        private string crashfile;
+
+        private string droidpath;
+
+        private Process droidplay;
+
+        private Queue<string> queueShow = new Queue<string>();
+
+        public LogMain()
+        {
+            InitializeComponent();
+        }
+
+        private void LogMain_Load(object sender, EventArgs e)
+        {
+            this.droidpath = ConfigurationManager.AppSettings["adb"];
+            if (File.Exists(this.droidpath))
+            {
+                if (!Directory.Exists(this.filestore = Environment.CurrentDirectory + "\\logcat\\"))
+                {
+                    Directory.CreateDirectory(this.filestore);
+                }
+                return;
+            }
+            else
+            {
+                MessageBox.Show("请正确配置ADB程序路径！");
+            }
+            Environment.Exit(0);
+        }
+
+        private void startListen(string filename, string filetime)
+        {
+            this.linecount = 0;
+            this.drawindex = 0;
+            this.findindex = 0;
+            this.writeLog.Clear();
+            try
+            {
+                if (this.droidplay == null || this.droidplay.HasExited)
+                {
+                    this.droidplay = new Process();
+                    this.droidplay.StartInfo.FileName = this.droidpath;
+                    this.droidplay.StartInfo.Arguments = "logcat";
+                    this.droidplay.EnableRaisingEvents = true;
+                    this.droidplay.StartInfo.CreateNoWindow = true;
+                    this.droidplay.StartInfo.UseShellExecute = false;
+                    this.droidplay.StartInfo.RedirectStandardError = true;
+                    this.droidplay.StartInfo.RedirectStandardOutput = true;
+                    this.droidplay.StartInfo.StandardErrorEncoding = Encoding.UTF8;
+                    this.droidplay.StartInfo.StandardOutputEncoding = Encoding.UTF8;
+                    this.droidplay.ErrorDataReceived += new DataReceivedEventHandler(adb_ErrorDataReceived);
+                    this.droidplay.OutputDataReceived += new DataReceivedEventHandler(adb_OutputDataReceived);
+                }
+            }
+            catch (Exception e)
+            {
+                this.closeListen();
+                Console.WriteLine("Start Listen init error: " + e.ToString());
+            }
+            finally
+            {
+                if (this.droidplay != null)
+                {
+                    try
+                    {
+                        this.droidplay.Start();
+                        this.droidplay.BeginErrorReadLine();
+                        this.droidplay.BeginOutputReadLine();
+                    }
+                    catch (Exception e)
+                    {
+                        this.closeListen();
+                        Console.WriteLine("Start Listen todo error: " + e.ToString());
+                    }
+                    finally
+                    {
+                        if (this.droidplay != null)
+                        {
+                            this.isListen = true;
+                            this.lockBtn.Enabled = true;
+                            this.logcatBtn.Enabled = true;
+                            this.writeTimer.Enabled = true;
+                            this.logcatBtn.Text = "停 止 监 听";
+                            this.logcatBtn.BackColor = Color.Red;
+                            this.logcatBtn.ForeColor = Color.Yellow;
+                            this.cachefile = this.filestore + filename + ".stdout_" + filetime + ".log";
+                            this.crashfile = this.filestore + filename + ".stderr_" + filetime + ".log";
+                        }
+                    }
+                }
+            }
+        }
+
+        private void clearListen()
+        {
+            this.logcatBtn.Enabled = false;
+            try
+            {
+                if (this.droidplay == null || this.droidplay.HasExited)
+                {
+                    this.droidplay = new Process();
+                    this.droidplay.StartInfo.FileName = this.droidpath;
+                    this.droidplay.StartInfo.Arguments = "logcat -c";
+                    this.droidplay.EnableRaisingEvents = true;
+                    this.droidplay.StartInfo.CreateNoWindow = true;
+                    this.droidplay.StartInfo.UseShellExecute = false;
+                }
+            }
+            catch (Exception e)
+            {
+                this.closeListen();
+                Console.WriteLine("Clear Listen init error: " + e.ToString());
+            }
+            finally
+            {
+                if (this.droidplay != null)
+                {
+                    try
+                    {
+                        this.droidplay.Start();
+                    }
+                    catch (Exception e)
+                    {
+                        this.closeListen();
+                        Console.WriteLine("Clear Listen todo error: " + e.ToString());
+                    }
+                    finally
+                    {
+                        if (this.droidplay != null)
+                        {
+                            try
+                            {
+                                this.droidplay.WaitForExit(30000);
+                            }
+                            catch (Exception e)
+                            {
+                                Console.WriteLine("Clear Listen wait error: " + e.ToString());
+                            }
+                            finally
+                            {
+                                this.closeListen();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void closeListen()
+        {
+            if (this.droidplay != null)
+            {
+                try
+                {
+                    this.killTreeProcess(this.droidplay.Id);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Close Listen todo error: " + e.ToString());
+                }
+                finally
+                {
+                    this.droidplay = null;
+                }
+            }
+            this.isListen = false;
+            this.queueShow.Clear();
+            this.lockBtn.Enabled = false;
+            this.logcatBtn.Enabled = true;
+            this.writeTimer.Enabled = false;
+            this.logcatBtn.Text = "开 始 监 听";
+            this.logcatBtn.BackColor = Color.Green;
+            this.logcatBtn.ForeColor = Color.White;
+        }
+
+        private void killTreeProcess(int pid)
+        {
+            try
+            {
+                Process cmd = Process.GetProcessById(pid);
+                if (cmd != null)
+                {
+                    cmd.Kill();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Close Listen tree error: " + e.ToString());
+            }
+            finally
+            {
+                ManagementObjectSearcher mos = new ManagementObjectSearcher("Select ProcessID From Win32_Process Where ParentProcessID=" + pid);
+                if (mos != null)
+                {
+                    ManagementObjectCollection moc = mos.Get();
+                    if (moc != null && moc.Count > 0)
+                    {
+                        foreach (ManagementObject moo in moc)
+                        {
+                            if (moo != null)
+                            {
+                                this.killTreeProcess(Convert.ToInt32(moo["ProcessID"]));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void lockBtn_Click(object sender, EventArgs e)
+        {
+            if (!this.isLocker)
+            {
+                this.isLocker = true;
+                this.lockBtn.Image = global::PCLogcat.Properties.Resources.onlock;
+            }
+            else
+            {
+                this.isLocker = false;
+                this.lockBtn.Image = global::PCLogcat.Properties.Resources.unlock;
+            }
+        }
+
+        private void logcatBtn_Click(object sender, EventArgs e)
+        {
+            if (this.isListen)
+            {
+                if (this.isLocker)
+                {
+                    this.lockBtn.PerformClick();
+                }
+                this.closeListen();
+            }
+            else
+            {
+                String filename = Interaction.InputBox("请输入名称：", "日志名称", "", -1, -1);
+                if (!"".Equals(filename))
+                {
+                    this.closeListen();
+                    this.clearListen();
+                    this.startListen(filename, DateTime.Now.ToFileTime() + "");
+                }
+            }
+        }
+
+        private void queryBtn_Click(object sender, EventArgs e)
+        {
+            if (this.isSearch)
+            {
+                this.findindex = 0;
+                this.drawindex = 0;
+                this.queryTxt.Text = "";
+                this.queryTxt.Cursor = Cursors.IBeam;
+                this.queryTxt.ReadOnly = (this.isSearch = false);
+                this.adb_ColourLogDataReceived();
+            }
+            else
+            {
+                if (!String.IsNullOrWhiteSpace(this.queryTxt.Text))
+                {
+                    this.queryTxt.Cursor = Cursors.Default;
+                    this.queryTxt.ReadOnly = (this.isSearch = true);
+                }
+                this.adb_SearchLogDataReceived();
+            }
+            this.queryBtn.Text = this.isSearch ? "停 止 查 找" : "开 始 查 找";
+        }
+
+        private void writeLog_Focus(object sender, EventArgs e)
+        {
+            this.queryTxt.Focus();
+        }
+
+        private void adb_ColourLogDataReceived()
+        {
+            MatchCollection match = LEVEL_REGEX.Matches(this.writeLog.Text, this.drawindex);
+            if (match != null && match.Count > 0)
+            {
+                foreach (Match rs in match)
+                {
+                    if (rs != null && rs.Success)
+                    {
+                        this.drawindex = rs.Index + rs.Length;
+                        this.writeLog.Select(rs.Index, rs.Length);
+                        this.writeLog.SelectionBackColor = Color.Black;
+                        this.writeLog.SelectionFont = new Font(
+                            this.writeLog.Font, FontStyle.Regular
+                        );
+                        switch (rs.Groups[1].Value)
+                        {
+                            case "W":
+                                {
+                                    this.writeLog.SelectionColor = Color.Yellow;
+                                    break;
+                                }
+                            case "D":
+                                {
+                                    this.writeLog.SelectionColor = Color.Green;
+                                    break;
+                                }
+                            case "E":
+                                {
+                                    this.writeLog.SelectionColor = Color.Red;
+                                    break;
+                                }
+                            default:
+                                {
+                                    this.writeLog.SelectionColor = Color.White;
+                                    break;
+                                }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void adb_SearchLogDataReceived()
+        {
+            if (this.isSearch)
+            {
+                Regex regex = new Regex(this.queryTxt.Text, RegexOptions.IgnoreCase);
+                if (regex != null)
+                {
+                    MatchCollection match = regex.Matches(this.writeLog.Text, this.findindex);
+                    if (match != null && match.Count > 0)
+                    {
+                        foreach (Match rs in match)
+                        {
+                            if (rs != null && rs.Success)
+                            {
+                                this.findindex = rs.Index + rs.Length;
+                                this.writeLog.Select(rs.Index, rs.Length);
+                                this.writeLog.SelectionColor = Color.Black;
+                                this.writeLog.SelectionBackColor = Color.Yellow;
+                                this.writeLog.SelectionFont = new Font(this.writeLog.Font, FontStyle.Bold);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private string adb_FormatOutDataReceived(string data)
+        {
+            StringBuilder format = new StringBuilder(2048);
+            if (!String.IsNullOrWhiteSpace(data))
+            {
+                format.Append("[").Append(((++this.linecount) + "").PadLeft(10, '-')).Append("] | ").Append(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss:fff")).Append(" - ").Append(data.Trim()).Append(" ").Append(Environment.NewLine);
+            }
+            return format.ToString();
+        }
+
+        private void adb_ErrorDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            if (!this.InvokeRequired)
+            {
+                string stderr = this.adb_FormatOutDataReceived(e.Data);
+                if (!String.IsNullOrWhiteSpace(stderr))
+                {
+                    File.AppendAllText(this.crashfile, stderr);
+                }
+            }
+            else
+            {
+                this.Invoke(new Action<object, DataReceivedEventArgs>(adb_ErrorDataReceived), sender, e);
+            }
+        }
+
+        private void adb_OutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            if (!this.InvokeRequired)
+            {
+                string stdout = this.adb_FormatOutDataReceived(e.Data);
+                if (!String.IsNullOrWhiteSpace(stdout))
+                {
+                    this.queueShow.Enqueue(stdout);
+                }
+            }
+            else
+            {
+                this.Invoke(new Action<object, DataReceivedEventArgs>(adb_OutputDataReceived), sender, e);
+            }
+        }
+
+        private void writeTimer_Tick(object sender, EventArgs e)
+        {
+            if (this.isListen && this.queueShow.Count > 0)
+            {
+                StringBuilder output = new StringBuilder(5120);
+                for (int i = 0; i < 300; i++)
+                {
+                    if (this.queueShow.Count > 0)
+                    {
+                        output.Append(this.queueShow.Dequeue());
+                        continue;
+                    }
+                    break;
+                }
+                // 文本输出
+                string append = output.ToString();
+                try
+                {
+                    File.AppendAllText(this.cachefile, append);
+                }
+                catch (Exception)
+                {
+                    append = null;
+                }
+                finally
+                {
+                    if (append != null)
+                    {
+                        this.writeLog.AppendText(append);
+                        this.adb_ColourLogDataReceived();
+                        this.adb_SearchLogDataReceived();
+                        if (!this.isLocker)
+                        {
+                            this.writeLog.ScrollToCaret();
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
